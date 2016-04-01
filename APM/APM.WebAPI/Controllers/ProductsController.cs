57efcf9d618aa57a4ef1177d18f8f55ea2159a2e
Sean McNary <smcnary@live.com>
2016-03-31 22:12:02 -0500

    Using Odata for Webapi queries.

---
+++ APM/APM.WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,53 @@
+﻿using APM.WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.OData;
+
+namespace APM.WebAPI.Controllers
+{
+    [EnableCorsAttribute("http://localhost:52436", "*", "*")]
+    public class ProductsController : ApiController
+    {
+        // GET: api/Products
+        [EnableQuery()]
+        public IQueryable<Product> Get()
+        {
+            var productRepository = new ProductRepository();
+            return productRepository.Retrieve().AsQueryable();
+        }
+
+        public IEnumerable<Product> Get(string search)
+        {
+            var productRepository = new ProductRepository();
+            var products = productRepository.Retrieve();
+            return products.Where(p => p.ProductCode.Contains(search));
+        }
+
+        
+        // GET: api/Products/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Products
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Products/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Products/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
